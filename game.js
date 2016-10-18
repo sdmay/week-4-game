e31b4a0f8bc4ba72032111ebde7f1a1243132300
sdmay10 <sdmay10@gmail.com>
2016-10-18 17:17:56 -0400

    P

--- game.js
+++ game.js
@@ -5,7 +5,52 @@ var match = 0;
 var score = 0;
 var win = 0;
 var loss = 0;
+var randomNumber = [];
 
-$('.win').text(win)
-$('.loss').text(loss)
+
+$('.win').html(win)
+$('.loss').html(loss)
+
+	function newGame() {
+
+		counter = 0;
+		$('.score').text(counter);
+
+		function madUpNumber(){
+		   	return Math.floor(Math.random()*(max-min+1)+min);
+			}
+
+		var numberToGuess = randomIntFromInterval(19,120);
+
+		$('.match').text(numberToGuess);
+
+
+		$('.gem').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   
+		    $('#yourScore').text(counter);
+
+		    if (counter == numberToGuess){
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      console.log(wins)
+		      $('.gem').empty();
+		      newCrystals();
+		      newGame();
+		        
+		    } else if ( counter > numberToGuess){
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('.loss').text(losses);
+		        console.log(losses)
+		        $('.gem').empty();
+		        newCrystals();
+		        newGame();
+		    }
+		});
+	}
+
+};
+
 
