0cb6067fd4b23fafbe6739d0dff97271fce86b4a
sdmay10 <sdmay10@gmail.com>
2016-10-18 16:52:16 -0400

    L

---
+++ game.js
@@ -0,0 +1,11 @@
+// Crystal Array,
+crystals = ["images/sapphire.jpg", "images/amethyst.jpg", "images/ruby.png", "images/opal.jpg"];
+
+var match = 0;
+var score = 0;
+var win = 0;
+var loss = 0;
+
+$('.win').text(win)
+$('.loss').text(loss)
+
